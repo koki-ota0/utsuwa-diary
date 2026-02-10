3fd5e58a4d62aafdeb9d8562f2f3562f35e429ca
koki-ota0 <85487709+koki-ota0@users.noreply.github.com>
2026-02-11 01:52:36 +0900

    Initialize Vite React TypeScript project scaffold

---
+++ src/components/forms/index.ts
@@ -0,0 +1,1 @@
+// Re-export form components from this directory.
---
+++ src/components/layout/PageLayout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react'
+
+function PageLayout({ children }: PropsWithChildren) {
+  return <main>{children}</main>
+}
+
+export default PageLayout
